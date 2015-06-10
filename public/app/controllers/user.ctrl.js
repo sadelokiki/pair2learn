5b10c6c36949b215474ef4a2d201196b771ae542
Susan <susan.adelokiki@andela.co>
2015-06-10 18:25:27 +0100

    Implement passport-local sign up feature

--- public/app/controllers/user.ctrl.js
+++ public/app/controllers/user.ctrl.js
@@ -1,7 +1,15 @@
 'use strict';
 
 angular.module('pairToLearnApp')
-  .controller('UserCtrl', [function($scope) {
+  .controller('UserCtrl', ['UserService', '$scope', '$location', function(UserService, $scope, $location) {
+   
+    $scope.signUp = function(user) {
+      console.log(user)
+      UserService.signUp(user).then(function(data){
+        console.log(data)
+      })
+    };
+
     (function($){
       $(function(){
         $('.parallax').parallax();
