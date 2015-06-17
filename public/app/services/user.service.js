f3e92af6d75e069b0233180496cdec495584f54f
Kinglsey Ochu <kingsley.ochu@andela.co>
2015-06-17 10:22:18 +0100

    remove unnecessary service calls

--- public/app/services/user.service.js
+++ public/app/services/user.service.js
@@ -4,11 +4,16 @@ angular.module('pairToLearnApp')
   .factory('UserService', ['$http', function($http) {
     var baseUrl = "http://localhost:3000";
     var User = {
+<<<<<<< HEAD
       update: function(id, params) {
         return $http.put(baseUrl + '/users/' + id, params).then(function(data) {
            return data;
         });
       }
+=======
+     
+>>>>>>> remove unnecessary service calls
     };
+    
     return User;
   }]);
