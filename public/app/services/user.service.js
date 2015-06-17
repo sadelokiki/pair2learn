4992fa140b5808a1de5734574cf814518bf963f7
Kinglsey Ochu <kingsley.ochu@andela.co>
2015-06-17 04:13:45 +0100

    remove sigin and signup services as they have been replaced by passport

--- public/app/services/user.service.js
+++ public/app/services/user.service.js
@@ -4,20 +4,9 @@ angular.module('pairToLearnApp')
   .factory('UserService', ['$http', function($http) {
     var baseUrl = "http://localhost:3000";
     var User = {
-      // signUp: function(data) {
-      //   return $http.post(baseUrl + '/signup', data).then(function(data){
-      //     console.log(data);
-      //     return data;
-      //   });
-      // },
-      // signIn: function(data) {
-      //   return $http.post(baseUrl + '/login', data).then(function(data) {
-      //     return data;
-      //   });
-      
       update: function(id, params) {
         return $http.put(baseUrl + '/users/' + id, params).then(function(data) {
-          return data;
+           return data;
         });
       }
     };
