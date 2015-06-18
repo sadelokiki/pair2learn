ccf507694db98d807c0d5831e19a46685f12be59
Kinglsey Ochu <kingsley.ochu@andela.co>
2015-06-18 09:45:41 +0100

    move firepad script to Pair Controller [Refinishes #97175714]

--- public/app/controllers/pair.ctrl.js
+++ public/app/controllers/pair.ctrl.js
@@ -5,7 +5,29 @@ angular.module('pairToLearnApp')
     (function($){
       $(function(){
         $('.parallax').parallax(); 
+        $('.collapsible').collapsible({
+		      accordion : true // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+		    });
       });
     })(jQuery);
+
+    var firepadRef = new Firebase('https://pairtolearn.firebaseio.com/');
+
+  	var codeMirror = CodeMirror(document.getElementById('firepad'), 
+      { 
+        lineWrapping: false 
+      });
+
+  	var firepad = Firepad.fromCodeMirror(firepadRef, codeMirror,
+      { 
+        richTextShortcuts: true,
+        richTextToolbar: true, 
+        userId: 'fdhhfd',
+        defaultText: 'Type Live text here'
+      });
+	  firepad.on('ready', function() {
+	  // Firepad is ready.
+
+	  });
 
   }]);
