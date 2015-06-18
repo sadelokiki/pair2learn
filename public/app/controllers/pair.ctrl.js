d7dd7c886e2de94e0c6ef1c93f03ac7257cc5981
Kinglsey Ochu <kingsley.ochu@andela.co>
2015-06-18 08:25:52 +0100

    add link to codemirror to the index.hmtl and set height for the code editor in custom css [#97096556]

---
+++ public/app/controllers/pair.ctrl.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('pairToLearnApp')
+  .controller('PairCtrl', ['$rootScope', '$scope', '$location', function($rootScope, $scope, $location) {
+    (function($){
+      $(function(){
+        $('.parallax').parallax(); 
+      });
+    })(jQuery);
+
+  }]);
