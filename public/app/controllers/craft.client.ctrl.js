7ca58a2c7357330c1f0a47492f88769e15330da5
Kinglsey Ochu <kingsley.ochu@andela.co>
2015-06-20 14:36:03 +0100

    change the post a craft to use the craft service [#97017112]

--- public/app/controllers/craft.client.ctrl.js
+++ public/app/controllers/craft.client.ctrl.js
@@ -1,7 +1,7 @@
 "use strict";
 
 angular.module('pairToLearnApp')
-  .controller('CraftCtrl', ['CraftService', '$scope', '$location', '$timeout', '$rootScope', 'Upload','$window', function(CraftService, $scope, $location, $timeout, $rootScope, Upload, $window) {    
+  .controller('CraftCtrl', ['CraftService', '$scope', '$location', '$timeout', '$rootScope', 'Upload', '$window', function(CraftService, $scope, $location, $timeout, $rootScope, Upload, $window) {    
     (function($) {
       $(function() {
         $('.parallax').parallax();
@@ -10,26 +10,21 @@ angular.module('pairToLearnApp')
     })(jQuery);
 
     $scope.createCraft = function(craft) {
-      var user_id = $rootScope.decodedToken.user._id
-      craft.createdBy = user_id;
-      var localhost = "http://localhost:3000/crafts";
-      var upload = Upload.upload({
-          url: localhost,
-          method: "POST",
-          file: craft.picture,
-          fields: craft
-        })
-        .success(function(data) {
-          console.log(data);
-          $location.url("/user/" + data._id);
-        });
+      if(!craft) {
+        return;
       }
+      var user_id = $rootScope.decodedToken.user._id;
+      craft.createdBy = user_id;
+      $rootScope.showProg = true;
+      CraftService.createCraft(craft.picture, craft).then(function(data) {
+        Materialize.toast('Craft created successfully!', 4000);
+        console.log(data);
+        $rootScope.showProg = false;
+        $location.url("/user/" + data._id);
+      });
+    };
 
     CraftService.getAll().then(function(data) {
-      console.log(data);
       $scope.allCrafts = data;
-      console.log($scope.allCrafts);
-      return $scope.allCrafts;
     });
-
-}])
+  }]);
