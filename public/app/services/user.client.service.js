6fb1804b7c6ccdd7b92c7b179757f4b6d1d6386c
Kinglsey Ochu <kingsley.ochu@andela.co>
2015-06-20 12:55:20 +0100

    Create profile picture service [#97175714]

--- public/app/services/user.client.service.js
+++ public/app/services/user.client.service.js
@@ -1,15 +1,25 @@
 "use strict";
 
 angular.module('pairToLearnApp')
-  .factory('UserService', ['$http', function($http) {
+  .factory('UserService', ['$http', 'Upload', function($http, Upload) {
     var baseUrl = "http://localhost:3000";
     var User = {
-      update: function(id, params) {
-        return $http.put(baseUrl + '/users/' + id, params).then(function(data) {
-          return data;
-        });
-      }
     };
-
+    User.updateProfile = function(id, params) {
+      return $http.put(baseUrl + '/users/' + id, params).then(function(data) {
+         return data;
+      });
+    };
+    User.uploadPic = function(file, fields) {
+      return Upload.upload({
+            url: baseUrl + '/users',
+            method: "POST",
+            file: file,
+            fields: fields
+          })
+          .then(function(res) {
+            return res.data;
+          });
+    };
     return User;
   }]);
