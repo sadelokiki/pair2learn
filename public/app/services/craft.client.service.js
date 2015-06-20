2b6258195cf59836db6caaf1bfb4a58a8b981596
Kinglsey Ochu <kingsley.ochu@andela.co>
2015-06-20 14:37:59 +0100

    add the post craft service and remove dead code [#97017112]

--- public/app/services/craft.client.service.js
+++ public/app/services/craft.client.service.js
@@ -1,26 +1,33 @@
 "use strict";
 
 angular.module('pairToLearnApp')
-  .factory('CraftService', ['$http', function($http) {
+  .factory('CraftService', ['$http', 'Upload', function($http, Upload) {
     var baseUrl = "http://localhost:3000";
-    return {
-    post: function(data) {
-      return $http.post(baseUrl + '/crafts/', data).then(function(res) {
+    var crafts = {};
+
+    crafts.createCraft = function(file, craft) {
+      return Upload.upload({
+          url: baseUrl + '/crafts',
+          method: "POST",
+          file: file,
+          fields: craft
+      })
+      .then(function(res) {
         return res.data;
       });
-    },
+    };
 
-    getOne: function(id, params) {
-      return $http.get(baseUrl + '/crafts/' + id, data).then(function(res) {
+    crafts.getOne = function(id) {
+      return $http.get(baseUrl + '/crafts/' + id).then(function(res) {
         return res.data;
       });
-    },
+    };
 
-    getAll: function() {
+    crafts.getAll = function() {
       return $http.get(baseUrl + '/crafts/').then(function(res) {
         return res.data;
-      })
-    }
-  };
-    
-  }])
+      });
+    };
+
+    return crafts;
+  }]);
