3e0d6094b2b49b69029eca118e26fc87f44a8b34
Susan <susan.adelokiki@andela.co>
2015-06-23 21:43:38 +0100

    Refactor wallet layout Refactor toast layout Refactor User period from hours to minutes

--- public/app/controllers/wallet.client.ctrl.js
+++ public/app/controllers/wallet.client.ctrl.js
@@ -5,11 +5,29 @@ angular.module('pairToLearnApp')
     $timeout(function() {
       console.log($rootScope);
       UserService.getOneUser($rootScope.decodedToken.user._id).then(function(data) {
-        $scope.hours = data.hours;
+        $scope.minutes = data.minutes;
         // $rootScope.hours = data.hours;
       });
     }, 1000);
+    //UserService.getOneUser($rootScope.decodedToken.user._id).then(function(data) {
+      $scope.counter = $scope.minutes * 60;
+   // });
 
+    var mytimeout = null;
+    $scope.onTimeout = function() {
+      if ($scope.counter === 0) {
+        $scope.$broadcast('timer-stopped', 0);
+        $timeout.cancel(mytimeout);
+        return;
+      }
+      $scope.counter--;
+      mytimeout = $timeout($scope.onTimeout, 1000);
+    }
+    $scope.startTimer = function() {
+      console.log("time starts now");
+      mytimeout = $timeout($scope.onTimeout, 1000);
+    }
+    
     $scope.craftId = $window.sessionStorage.craft;
     $scope.expertId = $window.sessionStorage.expert;
   }]);
