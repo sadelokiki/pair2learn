7705de4a6229c301f4284dd88e79477b9c850bde
Kinglsey Ochu <kingsley.ochu@andela.co>
2015-06-23 14:17:35 +0100

    remove add craft function from this controller

--- public/app/controllers/craft.client.ctrl.js
+++ public/app/controllers/craft.client.ctrl.js
@@ -9,24 +9,6 @@ angular.module('pairToLearnApp')
       });
     })(jQuery);
 
-    $scope.createCraft = function(craft) {
-      if (!craft) {
-        return;
-      }
-      if (!$scope.craft.picture) {
-        return;
-      }
-      var user = $rootScope.decodedToken.user.firstname + " " + $rootScope.decodedToken.user.lastname;
-      craft.createdBy = user;
-      $rootScope.showProg = true;
-      CraftService.createCraft(craft.picture, craft).then(function(data) {
-        Materialize.toast('Craft created successfully!', 4000);
-        $rootScope.showProg = false;
-        $location.url("/user/" + data._id);
-      }, function(err) {
-        $rootScope.showProg = false;
-      });
-    };
 
     $scope.applyAsExpert = function(craftId) {
       var data = {
