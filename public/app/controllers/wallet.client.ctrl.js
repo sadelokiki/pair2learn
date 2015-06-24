9ef654d16d87d0a171e7f8d45ec27efe33736580
Kinglsey Ochu <kingsley.ochu@andela.co>
2015-06-24 11:21:44 +0100

    update wallet view

--- public/app/controllers/wallet.client.ctrl.js
+++ public/app/controllers/wallet.client.ctrl.js
@@ -2,16 +2,14 @@
 
 angular.module('pairToLearnApp')
   .controller('WalletCtrl', ['$rootScope', '$scope', '$window', '$location', 'UserService', '$timeout', function($rootScope, $scope, $window, $location, UserService, $timeout) {
+
     $timeout(function() {
-      console.log($rootScope);
       UserService.getOneUser($rootScope.decodedToken.user._id).then(function(data) {
-        $scope.minutes = data.minutes;
-        // $rootScope.hours = data.hours;
+        console.log(data)
+        $rootScope.minutes = data.minutes;
       });
     }, 1000);
-    //UserService.getOneUser($rootScope.decodedToken.user._id).then(function(data) {
-      $scope.counter = $scope.minutes * 60;
-   // });
+    $scope.counter = $scope.minutes * 60;
 
     var mytimeout = null;
     $scope.onTimeout = function() {
@@ -22,12 +20,12 @@ angular.module('pairToLearnApp')
       }
       $scope.counter--;
       mytimeout = $timeout($scope.onTimeout, 1000);
-    }
+    };
     $scope.startTimer = function() {
       console.log("time starts now");
       mytimeout = $timeout($scope.onTimeout, 1000);
-    }
-    
+    };
+
     $scope.craftId = $window.sessionStorage.craft;
     $scope.expertId = $window.sessionStorage.expert;
   }]);
