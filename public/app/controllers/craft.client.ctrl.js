556e063c16a29e1dc0cf9aa28c23bb530a3cd03d
Susan <susan.adelokiki@andela.co>
2015-06-25 21:34:29 +0100

    Delete profile function

--- public/app/controllers/craft.client.ctrl.js
+++ public/app/controllers/craft.client.ctrl.js
@@ -4,6 +4,7 @@ angular.module('pairToLearnApp')
   .controller('CraftCtrl', ['CraftService', 'UserService', '$scope', '$location', '$timeout', '$rootScope', '$window', '$routeParams', function(CraftService, UserService, $scope, $location, $timeout, $rootScope, $window, $routeParams) {
     (function($) {
       $(function() {
+        $('.modal-trigger').leanModal();
         $('.parallax').parallax();
         $('.scrollspy').scrollSpy();
       });
@@ -37,6 +38,16 @@ angular.module('pairToLearnApp')
       var userId = $window.sessionStorage.user;
       $window.sessionStorage.craft = craftId;
       $location.url('/user/' + userId + '/craft/' + craftId);
+    };
+
+    $scope.deleteProfile = function(userId) {
+      console.log("time to go dear");
+      $window.sessionStorage.user = $rootScope.decodedToken.user._id;
+      UserService.deleteUser($rootScope.decodedToken.user._id).then(function(res) {
+        Materialize.toast('Profile deleted!', 4000);
+        $window.sessionStorage.clear();
+        $location.url('/home');
+      })
     };
 
 
