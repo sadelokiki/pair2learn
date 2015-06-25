70472a9bd3d972ff511b72f5f6ef95c17a215b8e
Susan <susan.adelokiki@andela.co>
2015-06-25 19:38:01 +0100

    Create Session service

--- public/app/controllers/wallet.client.ctrl.js
+++ public/app/controllers/wallet.client.ctrl.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('pairToLearnApp')
-  .controller('WalletCtrl', ['$rootScope', '$scope', '$window', '$location', 'UserService', '$timeout', function($rootScope, $scope, $window, $location, UserService, $timeout) {
+  .controller('WalletCtrl', ['$rootScope', '$scope', '$window', '$location', 'UserService', '$timeout', 'SessionService', function($rootScope, $scope, $window, $location, UserService, $timeout, SessionService) {
 
     $timeout(function() {
       UserService.getOneUser($rootScope.decodedToken.user._id).then(function(data) {
@@ -23,9 +23,11 @@ angular.module('pairToLearnApp')
     };
 
     $scope.bookMentorSession = function(userId, craftId) {
-      console.log(userId, 'id', craftId);
-
-    }
+      var expertId = $scope.expertId;
+      SessionService.sendMail(userId, craftId, expertId).then(function(data) {
+        console.log(data);
+      });
+    };
 
     $scope.userId = $window.sessionStorage.user;
     $scope.craftId = $window.sessionStorage.craft;
