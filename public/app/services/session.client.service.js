3b824c8ad6ded7e76039e1f5e00d0ace7a4ba68a
Susan <susan.adelokiki@andela.co>
2015-06-25 19:38:38 +0100

    Add service path to html

--- public/app/services/session.client.service.js
+++ public/app/services/session.client.service.js
@@ -1,14 +1,26 @@
 "use strict";
 
 angular.module('pairToLearnApp')
-  .factory('SessionService', ['$http', function($http) {
-    var session = {}
+  .factory('SessionService', ['$http', 'baseUrl', function($http, baseUrl) {
+    var Session = {}
 
-    session.bookSession = function () {
+    Session.bookSession = function () {
         return $http.get(baseUrl + '/pair/' + craftId + ' /' + userId).then(function(res) {
         return res.data;
       });
     }
 
+    Session.sendMail = function (userId, craftId, expertId) {
+      var mailDetails = {
+        userId: userId,
+        craftId: craftId,
+        expertId: expertId
+      }
+      
+      return $http.post(baseUrl + '/mail', mailDetails).then(function(res) {
+        return res.data;
+      });
+    }
+    return Session;
 
   }])
