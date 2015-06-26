3055faf3788d1c82b394698a3c4092f549cb29ba
Kinglsey Ochu <kingsley.ochu@andela.co>
2015-06-26 08:05:26 +0100

    add hide book when the user is just accessing his account

--- public/app/controllers/user.client.ctrl.js
+++ public/app/controllers/user.client.ctrl.js
@@ -13,6 +13,4 @@ angular.module('pairToLearnApp')
       });
     })(jQuery);
 
-
-
   }]);
--- public/app/controllers/wallet.client.ctrl.js
+++ public/app/controllers/wallet.client.ctrl.js
@@ -2,10 +2,23 @@
 
 angular.module('pairToLearnApp')
   .controller('WalletCtrl', ['$rootScope', '$scope', '$window', '$location', 'UserService', '$timeout', 'SessionService', function($rootScope, $scope, $window, $location, UserService, $timeout, SessionService) {
-
+    (function($) {
+      $(function() {
+        $('.datepicker').pickadate({
+          selectMonths: true, // Creates a dropdown to control month
+          selectYears: 15 // Creates a dropdown of 15 years to control year
+        });
+      });
+    })(jQuery);
+    var craftId = $window.sessionStorage.craft;
+    var expertId = $window.sessionStorage.expert;
+    $scope.showBookSession = true;
+    if (!craftId && !expertId) {
+      $scope.showBookSession = false;
+    }
     $timeout(function() {
       UserService.getOneUser($rootScope.decodedToken.user._id).then(function(data) {
-        console.log(data)
+        console.log(data);
         $scope.minutes = data.minutes;
       });
     }, 1000);
@@ -19,17 +32,22 @@ angular.module('pairToLearnApp')
         return;
       }
       $scope.counter--;
-     mytimeout = $timeout($scope.onTimeout, 1000);
+      mytimeout = $timeout($scope.onTimeout, 1000);
     };
 
-    $scope.bookMentorSession = function(userId, craftId) {
-      var expertId = $scope.expertId;
-      SessionService.sendMail(userId, craftId, expertId).then(function(data) {
+    $scope.bookMentorSession = function(userId, craftId, sessionData) {
+      if (!sessionData.date) {
+        return;
+      }
+      var expertId = $window.sessionStorage.expert;
+      SessionService.sendMail(userId, craftId, expertId, sessionData).then(function(data) {
+        Materialize.toast('Session Booked successfully!', 4000);
         console.log(data);
+        $location.path('/mycrafts');
       });
     };
+    // $scope.userId = $window.sessionStorage.user;
+    // $scope.craftId = $window.sessionStorage.craft;
+    // $scope.expertId = $window.sessionStorage.expert;
 
-    $scope.userId = $window.sessionStorage.user;
-    $scope.craftId = $window.sessionStorage.craft;
-    $scope.expertId = $window.sessionStorage.expert;
   }]);
